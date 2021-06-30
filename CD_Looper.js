8b47251adcb6d35655ad8c13d5c1954cfffde3bf
CPMDude89 <cdbasses@gmail.com>
2021-06-30 15:19:13 +0100

    new updates

---
+++ CD_Looper.js
@@ -0,0 +1,105 @@
+class Looper {
+    constructor(
+        //  get variable input from user
+        buttonX,    //  button x-coordinate
+        buttonY,    //  button y-coordinate
+        buttonWidth,    //  button width
+        buttonHeight    //  button height
+    ) {
+        //  define properties
+        this.buttonX = buttonX; //  properties from user input
+        this.buttonY = buttonY;
+        this.buttonWidth = buttonWidth;
+        this.buttonHeight = buttonHeight;
+
+        this.mic = new p5.AudioIn();    //  user input source (computer mic)
+        this.recorder = new p5.SoundRecorder(); //  p5 sound recorder object
+        this.soundFile = new p5.SoundFile();    //  p5 SoundFile object for audio buffer
+        this.state = 0; //  'state' variable used to control button functions through recording -> playback
+        this.ampModOsc = new p5.Oscillator();
+        this.delay = new p5.Delay();
+
+        this.button = createButton('START RECORD'); //  create button
+        this.button.mousePressed(() => this.record());  //  when button is clicked, start record process
+
+        this.clearButton;
+        this.delayButton;
+    }
+
+    //  set everything up
+    init() {
+        this.mic.start();   //  set up input source
+        this.recorder.setInput(this.mic);   //  connect microphone to recorder object
+
+        this.button.position(this.buttonX, this.buttonY);   //  set up recorder button
+        this.button.size(this.buttonWidth, this.buttonHeight);
+    }
+    
+    //  use rec button to record voice and play back 
+    record() {
+        userStartAudio();   //  ensure audio is good to go
+
+        if (this.state == 0 && this.mic.enabled) {    //  if nothing has been recorded yet and input is OK
+        //  record into p5.SoundFile after 120 milliseconds to get past mouse click
+        setTimeout(() => this.recorder.record(this.soundFile, 10), 120);
+        
+        this.button.html('STOP RECORDING')  //  change button text
+        this.state++;   //  change state to move to play back
+        }
+
+        else if (this.state == 1) {    //  stop recorder and send result to soundFile
+            this.recorder.stop();   //  stop recorder
+
+            this.button.html('PLAY');
+            this.state++;
+            this.button.size(this.buttonWidth/2, this.buttonHeight);
+
+            this.addClearButton();
+
+        }
+
+        else if (this.state == 2) { //  play back recording
+            this.soundFile.loop();
+            this.soundFile.jump(0);
+
+            this.button.html('STOP');
+            this.state++;
+
+            this.ampModOsc.start();
+            this.ampModOsc.disconnect();
+            this.ampModOsc.freq(20);
+            this.ampModOsc.amp(1);
+
+            //this.soundFile.setVolume(this.ampModOsc.scale(-1, 1, 0, 1));
+
+        }
+
+        else if(this.state == 3) {  //  stop playback
+            this.soundFile.stop();
+
+            this.button.html('PLAY');
+            this.state--;
+        }
+    }
+
+    addClearButton() {
+        this.clearButton = createButton('CLEAR');
+        this.clearButton.position(this.buttonX + (this.buttonWidth/2), this.buttonY);
+        this.clearButton.size(this.buttonWidth/2, this.buttonHeight);
+
+
+        this.clearButton.mousePressed(() => {  //  clear audio buffer and reset buttons
+            //console.log('asdl;kjasdfl;kjasdf');
+            this.soundFile.stop();
+            this.button.size(this.buttonWidth, this.buttonHeight);
+            this.soundFile = new p5.SoundFile();
+            this.button.html('START RECORD');
+            this.state = 0;
+            this.clearButton.remove();
+        });
+
+    }
+
+
+    
+}
--- oop_playingInTheWaves.js
+++ oop_playingInTheWaves.js
@@ -1,7 +1,7 @@
 let w = window.innerWidth;
 let h = window.innerHeight;
-let looper;
-let testBut;
+let looper1, looper2, looper3, looper4, looper5;
+let recButX=(4 * w/5), recButY=(h/6), recButWidth=(w/10), recButHeight=(h/10);
 
 function preload() {
         
@@ -11,15 +11,36 @@ function preload() {
 function setup() {
     createCanvas(w, h);
 
-    looper = new Looper(200, 200, 100, 100);  //  here we go with the first looper object
-    looper.init();  //  initialize button and recorder
+    looper1 = new Looper(recButX, recButY, recButWidth, recButHeight);  //  here we go with the first looper object
+    looper1.init();  //  initialize button and recorder
 
+    looper2 = new Looper(recButX, 2 * recButY, recButWidth, recButHeight);
+    looper2.init();  //  initialize button and recorder
 
+    looper3 = new Looper(recButX, 3 * recButY, recButWidth, recButHeight);
+    looper3.init();  //  initialize button and recorder
+
+    looper4 = new Looper(recButX, 4 * recButY, recButWidth, recButHeight);
+    looper4.init();  //  initialize button and recorder
+
+    looper5 = new Looper(recButX, 5 * recButY, recButWidth, recButHeight);
+    looper5.init();  //  initialize button and recorder
 }
 
 function draw() {
     background(128, 128, 255);    //  draw background
 
+    textAlign(CENTER);  //  title
+    textSize(45);
+    text('Playing In The Waves: A Play-Based Approach to Generative Sound Design', w/2, h/11);
+
+    textAlign(LEFT);    //  name of track 1
+    textSize(28);
+    text('Track 1', recButX, (recButY - (0.01 * recButY)));
+    text('Track 2', recButX, (2*recButY - (0.01 * 2 * recButY)));
+    text('Track 3', recButX, (3*recButY - (0.01 * 3 * recButY)));
+    text('Track 4', recButX, (4*recButY - (0.01 * 4 * recButY)));
+    text('Track 5', recButX, (5*recButY - (0.01 * 5 * recButY)));
 
 }
 
