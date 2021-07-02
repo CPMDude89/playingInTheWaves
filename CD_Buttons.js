6b5145547e8fce0c75a38e26f4637c93614590a8
CPMDude89 <cdbasses@gmail.com>
2021-07-02 11:53:36 +0100

    Renamed file

--- CD_Buttons.js
+++ CD_Buttons.js
@@ -1,13 +1,12 @@
 /**
  * Class to create and control graphic buttons in the p5 draw loop
  */
-class Button {
+class Buttons {
     //  constructor params
     constructor(    
         effButX,     //  effect button x-coordinate
         effButY,    //  effect button y-coordinate
         effButWidth,    //  effect button width
         effButHeight, //    effect button height
-        
     )
 }
