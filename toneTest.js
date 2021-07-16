dbf63fe584d8c7140fe6e9dfb5ce9a5f1f1e7321
CPMDude89 <cdbasses@gmail.com>
2021-07-16 13:46:24 +0100

    Got it working in Tone!

--- toneTest.js
+++ toneTest.js
@@ -1,36 +1,111 @@
 let w = window.innerWidth;
 let h = window.innerHeight;
+let recBut, recButX=0.5 * w, recButY=0.5 * h, recButWd=0.2 * w, recButHt=0.2 * h;
+let playBut;
+let recActive = false;
+let mic, recorder, player, buffer;
+let data, bloburl;
+
+
+function setup() {
+  createCanvas(w, h);
+
+  recBut = createButton('RECORD');    //  setting up record button
+  recBut.position(recButX, recButY);
+  recBut.size(recButWd, recButHt);
+  recBut.mousePressed(toneRecord);
+
+  
+
+  mic = new Tone.UserMedia();   //  get microphone input
+  recorder = new Tone.Recorder();   //  set up Tone recorder object
+
+  mic.connect(recorder);    //  connect output of microphone to recorder object
+  mic.open();
+
+  buffer = new Tone.ToneAudioBuffer();
+
+}
+
+function draw() {
+    background(0, 100, 20);   //  nice dark green
+
+    
+
+}
+
+async function toneRecord() { //  here we go, recording with Tone.js
+  if (!recActive) {   //  activate recording
+    console.log('PRESSED TO ACTIVATE');
+
+    setTimeout(function() {recorder.start()}, 120);
+
+    recActive = true;
+  }
+  else {
+    console.log('PRESSED TO DEACTIVATE');   //  deactivate recording
+
+    data = await recorder.stop();
+    bloburl = URL.createObjectURL(data);
+
+    buffer = new Tone.ToneAudioBuffer(bloburl);
+
+    player = new Tone.Player(buffer).toDestination();
+
+
+    createButtons();
+
+
+    recActive = false;
+  }
+}
+
+function createButtons() {
+  playBut = createButton('PLAY');
+  playBut.position(recButX, 1.5 * recButY);
+  playBut.size(recButWd, recButHt);
+  playBut.mousePressed(function() {
+    player.start();
+  }) 
+}
+
+
+
+
+
+
+
+
+
+/*
 let testButton, testButtonX=0.5 * w, testButtonY=0.5 * h, testButtonWidth=0.1 * w, testButtonHeight=0.1 * h;
 let osc;
 let testToneActive = false;
 let lfo;
 let wave;
+*/
 
-function setup() {
-  createCanvas(w, h);
 
+/*  //  THIS WAS TEST CODE TO GET AMP MOD WORKING ---- IN SETUP()
 
   testButton = createButton('TEST TONE');
   testButton.position(testButtonX, testButtonY);
   testButton.size(testButtonWidth, testButtonHeight);
   testButton.mousePressed(testTone);
 
-  osc = new Tone.Oscillator().toDestination();  //  test oscillator
-  
 
+  osc = new Tone.Oscillator().toDestination();  //  test oscillator
   wave = new Tone.Waveform();
   wave.size = 2048;
   osc.connect(wave);
-
   lfo = new Tone.LFO(1, -60, 0);
   lfo.connect(osc.volume);
 
-  
-}
 
-function draw() {
-    background(0, 100, 20);
+*/
 
+
+/*    //  THIS WAS TEST CODE TO GET AMP MOD WORKING ---- IN DRAW()
     osc.frequency.value = map(mouseX, 0, w, 30, 500);
 
     stroke(255);
@@ -46,10 +121,9 @@ function draw() {
       vertex(x,y);
     }
     endShape();
+*/
 
-
-}
-
+/*
 function testTone() {
   if (!testToneActive) {
     console.log('BUTTON PRESSED');
@@ -67,4 +141,4 @@ function testTone() {
     testToneActive = false;
   }
 }
-
+*/
