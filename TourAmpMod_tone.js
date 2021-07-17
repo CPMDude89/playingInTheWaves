d03395f672c3f39ec0f65ea7d1a684decece214a
CPMDude89 <cdbasses@gmail.com>
2021-07-17 23:34:15 +0100

    Tone Amp Mod!!!!

---
+++ TourAmpMod_tone.js
@@ -0,0 +1,209 @@
+/**
+ * A part of Playing In The Waves' 'Tour'
+ * This program will take in the user's input, and store it in a new audio buffer. 
+ * Then it can be played back, and the amplitude of buffer playback will be attached to a p5.Oscillator
+ * The phase of the LFO modulating the buffer's amplitude will be animated
+ *          to help visualize how an LFO can affect a sound's volume
+ * 
+ * Made with Tone.js!
+ */
+
+let w = window.innerWidth;
+let h = window.innerHeight;
+let mic, recorder, audioBuffer, recordButton, data, blob, player;
+let recButX=(0.11 * w), recButY=(0.2 * h), recButWd=(0.1 * w), recButHt=(0.1 * h);
+let state = 0;
+let lfoVizRectX=(0.85 * w), lfoVizRectY=(recButY), lfoVizRectWd=(0.03 * w), lfoVizRectHt=(0.6 * h);
+let lfoFreqSlider, sliderWd=(0.6 * w);
+let soundVizX=0.5 * w, soundVizY=0.7 * h, soundVizWd=0.45 * w, soundVizHt=0.5 * h;
+let testToneButton, testTone, testToneActive=false;
+let ampModButton, ampModLFO, ampModActive = false;
+let volNode;
+
+function setup() {
+    createCanvas(w, h);     //  make p5 canvas
+
+    volNode = new Tone.Volume().toDestination();
+
+    mic = new Tone.UserMedia();     //  set up microphone input
+    mic.open();     //  turn on audio input
+
+    recorder = new Tone.Recorder();     //  set up Tone recorder
+    mic.connect(recorder);      //  connect microphone output to Tone recorder object
+
+    recordButton = createButton('RECORD');      //  record button
+    recordButton.position(recButX, recButY);
+    recordButton.size(recButWd, recButHt);
+    recordButton.mousePressed(recordIn);    
+
+    testTone = new Tone.Oscillator(200, 'sine').connect(volNode);
+
+    testToneButton = createButton('TEST TONE');     //  sine wave test tone 
+    testToneButton.position(recButX, (0.5 * soundVizHt) + soundVizY - (recButHt));    //  sine wave helps the visualizer see the side bands
+    testToneButton.size(recButWd, recButHt);
+    testToneButton.mousePressed(triggerTestTone);
+
+    ampModLFO = new Tone.LFO(1, -100, 0);
+    ampModLFO.phase = 90;
+    ampModLFO.connect(volNode.volume);
+
+    ampModButton = createButton('ACTIVATE\nAMP MOD');
+    ampModButton.position((2 * recButWd) + recButX, recButY);
+    ampModButton.size(recButWd, recButHt);
+    ampModButton.mousePressed(triggerAmpMod);
+
+    lfoFreqSlider = createSlider(0.1, 50, 2, 0.1);      //  amp mod freq slider
+    lfoFreqSlider.size(sliderWd);
+    lfoFreqSlider.position(0.2 * w, 0.4 * h);
+    
+}
+
+function draw() {
+    background(0, 150, 80);     // nice shade of forest green
+
+    textAlign(CENTER);  //  set up page title
+    textSize(40);
+    fill(0);       
+    text('Playing In The Waves:\nAmplitude Modulation: Tone.js Version', 0.5 * w, 0.05 * h); //  page title
+
+    if (state == 1) {     //  if button is recording
+        fill(255, 0, 0);    //  red for record light
+        circle((recButX + (0.5 * recButWd)), (recButY - (0.4 * recButHt)), 0.4 * recButHt);
+    }
+
+    fill(0);
+    rectMode(CORNER);
+    rect(lfoVizRectX, lfoVizRectY, lfoVizRectWd, lfoVizRectHt);
+
+    if (ampModActive) {
+        ampModLFO.frequency.rampTo(lfoFreqSlider.value(), 0.05);
+        /*
+        recordButton.analyzer.setFreq(lfoFreqSlider.value());    //  continuously check for new frequency
+
+        let gain = recordButton.analyzer.process();  //  assign output of amp mod osc to variable    
+
+        
+
+        fill(100, 50, 150); //  nice purple color
+        //  set output of amp mod lfo to the y-axis of ball to visualize amp mod
+        circle((0.5 * lfoVizRectWd) + lfoVizRectX, map(gain, 0, 1, (lfoVizRectY + lfoVizRectHt), lfoVizRectY), 1.75 * lfoVizRectWd);
+        
+        fill(0);
+        textSize(40);
+        text('Amplitude modulation is at rate: ' + lfoFreqSlider.value() + ' Hz', 0.5 * w, 0.38 * h);
+        */
+    }
+
+    fill(0);    //  black
+    rectMode(CENTER);   //  align rectangle to center
+    rect(soundVizX, soundVizY, soundVizWd, soundVizHt);  //  create backdrop for waveform drawing
+
+    if (state > 1) {
+        /*
+        let spectrum = fft.analyze();
+        stroke(100, 50, 150)
+        
+        for (let i = 0; i < spectrum.length; i++) {
+            let x = map(i, 0, spectrum.length, soundVizX - (0.5 * soundVizWd), soundVizX + (0.5 * soundVizWd));
+
+            let y = map(spectrum[i], 0, 255, soundVizY + (0.5 * soundVizHt), soundVizY - (0.5 * soundVizHt));
+
+            line(x, soundVizY + (0.5 * soundVizHt), x, y);
+        }
+        
+        
+        
+        noFill();   //  set up for vertex
+        beginShape();
+        stroke(255);
+
+        for (let i = 0; i < wave.length; i++) {
+            let x = map(i, 0, wave.length, soundVizX - (0.5 * soundVizWd), soundVizX + (0.5 * soundVizWd));   //  get x value
+
+            let y = map(wave[i], -1, 1, soundVizY - soundVizHt, soundVizY + soundVizHt);     //  get y value
+
+            vertex(x, y);
+        }
+
+        endShape();
+        noStroke();
+        */
+    }
+
+}
+
+async function recordIn() {
+    if (state == 0) {       //  begin recording
+        setTimeout(function() {recorder.start()}, 120);     //  wait 120 ms to avoid mouse click then begin recording
+
+        recordButton.html('STOP RECORDING');        //  change button text
+        state = 1;      //  move record state through
+    }
+
+    else if (state == 1) {      //  stop recording
+        data = await recorder.stop();   //  end recording and return a javascript promise with the result in it
+        blob = URL.createObjectURL(data);   //  save the result of the recoder object into a blob and assign it a url object
+        audioBuffer = new Tone.ToneAudioBuffer(blob);    //  create a new audio buffer and assign it to the url object. Finally the recording is in an audio buffer
+        player = new Tone.Player(audioBuffer).connect(volNode);  //  connect recording to Tone player and route player to master output
+
+        showControls();
+
+        recordButton.html('PLAY RECORDING');    //  change button text
+        state = 2;
+    }
+
+    else if (state == 2) {      //  play recording
+        player.loop = true; //  set player to loop output
+        player.start();   //  play back recording
+
+        recordButton.html('STOP PLAYBACK');     //  change button text
+        state = 3;  //  more record state
+    }
+
+    else if (state == 3) {      //  stop playback
+        player.stop();    //    stop playing
+
+        recordButton.html('PLAY RECORDING');
+        state = 2;
+    }
+}
+
+function showControls() {
+
+    
+}
+
+function triggerTestTone() {
+    if (!testToneActive) {
+        testTone.start();
+
+        testToneActive = true;
+    }
+
+    else {
+        testTone.stop("+0.05");
+
+        testToneActive = false;
+    }
+}
+
+function triggerAmpMod() {
+    if (!ampModActive) {        //  trigger amp mod on all output
+        volNode.volume.rampTo(-100, 0.05);      //  bring down gain of volume node to avoid clipping
+
+        ampModLFO.start();      //  activate LFO to modulate volume
+
+        ampModActive = true;    //  flip button boolean
+    }
+
+    else {
+        volNode.volume.rampTo(0, 0.05);     //  bring volume node back up to normal volume
+        ampModLFO.amplitude.rampTo(0, 0.05);    //  bring LFO depth down gradually to avoid clicks as much as possible
+
+        ampModLFO.stop();       //  stop LFO
+        ampModLFO.phase = 90;   //  reset LFO phase back to safe place
+        ampModLFO.amplitude.rampTo(1, 0.3);     //  bring LFO depth back up 
+        
+        ampModActive = false;
+    }
+}
--- toneTest.js
+++ toneTest.js
@@ -1,12 +1,195 @@
 let w = window.innerWidth;
 let h = window.innerHeight;
+
+let testButton, testButtonX=0.5 * w, testButtonY=0.5 * h, testButtonWidth=0.1 * w, testButtonHeight=0.1 * h;
+let osc;
+let testToneActive = false;
+let lfo;
+let lfoButton, lfoActive=false;
+let tremolo;
+let wave;
+let lfoFreqSlider, lfoPhaseSlider;
+let gainNode, osc1;
+let volNode;
+
+
+function setup() {       //  THIS WAS TEST CODE TO GET AMP MOD WORKING ---- IN SETUP()
+  createCanvas(w, h);
+  
+  testButton = createButton('TEST TONE');
+  testButton.position(testButtonX, testButtonY);
+  testButton.size(testButtonWidth, testButtonHeight);
+  testButton.mousePressed(testTone);
+
+  lfoButton = createButton('AMP MOD');
+  lfoButton.position(testButtonX, 0.5 * testButtonY);
+  lfoButton.size(testButtonWidth, testButtonHeight);
+  lfoButton.mousePressed(triggerLFO);
+
+  volNode = new Tone.Volume(0).toDestination();
+
+  //osc = new Tone.Oscillator(220).toDestination();  //  test oscillator
+  osc = new Tone.Oscillator(220).connect(volNode);  //  test oscillator
+  //osc = new Tone.Oscillator(220);  //  test oscillator
+  
+  wave = new Tone.Waveform();
+  wave.size = 2048;
+  //osc.connect(wave);
+  volNode.connect(wave);
+
+  lfo = new Tone.LFO(1, -100, 0);
+  lfo.phase = 90;
+  //lfo.connect(osc.volume);
+  lfo.connect(volNode.volume);
+  //lfo.connect(Tone.getDestination().volume);
+
+  lfoFreqSlider = createSlider(0.1, 40, 1, 0.1);
+  lfoFreqSlider.position(0.3 * w, 0.75 * h);
+  lfoFreqSlider.size(w/3, 0.01 * h);
+
+  lfoPhaseSlider = createSlider(0, 360, 90, 0.01);
+  lfoPhaseSlider.position(0.3 * w, 0.85 * h);
+  lfoPhaseSlider.size(0.33 * w, 0.01 * h);
+
+
+  
+}
+
+
+
+
+function draw() {    //  THIS WAS TEST CODE TO GET AMP MOD WORKING ---- IN DRAW()
+  background(0, 100, 20);
+
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  let buffer = wave.getValue(0);
+  beginShape()
+
+  for (let i = 0; i < buffer.length; i++) {
+    let x = map(i, 0, buffer.length, 0, w);
+    let y = map(buffer[i], -1, 1, h, 0);
+
+    //point(x, y);
+    vertex(x,y);
+  }
+  endShape();
+  
+  //  test text
+  textSize(40);
+  fill(0);
+  textAlign(CENTER);
+  text('LFO FREQUENCY', 0.45 * w, 0.74 * h);
+  lfo.frequency.rampTo(lfoFreqSlider.value(), 0.05);
+
+  text('LFO PHASE', 0.45 * w, 0.84 * h);
+  //lfo.phase = lfoPhaseSlider.value();
+
+}
+
+function testTone() {
+  if (!testToneActive) {    //  trigger carrier signal
+    osc.start();
+    
+    console.log(osc.volume.value);
+
+    testToneActive = true;
+  }
+  else {
+    console.log('BUTTON PRESSED AGAIN');
+    osc.stop();
+
+    testToneActive = false;
+  }
+}
+
+function triggerLFO() {
+  if (!lfoActive) {   //  trigger modulating signal
+    //osc.volume.rampTo(-100, 0.1);
+    volNode.volume.rampTo(-100, 0.1);
+    //Tone.getDestination().volume.rampTo(-100, 0.1);
+    
+    lfo.start();
+    
+    //lfo.connect(osc.volume);
+
+    lfoActive = true;
+  }
+  else {
+    lfo.amplitude.rampTo(0, 0.1);
+    volNode.volume.rampTo(0, 0.1);
+
+    lfo.stop("+0.1");
+
+    lfo.amplitude.rampTo(1, 0.5);
+    lfo.phase = 90;
+    
+    //osc.volume.rampTo(0, 0.1);
+    
+    lfoActive = false;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let recBut, recButX=0.5 * w, recButY=0.5 * h, recButWd=0.2 * w, recButHt=0.2 * h;
 let playBut;
 let recActive = false;
 let mic, recorder, player, buffer;
 let data, bloburl;
 
-
+/*
 function setup() {
   createCanvas(w, h);
 
@@ -16,7 +199,6 @@ function setup() {
   recBut.mousePressed(toneRecord);
 
   
-
   mic = new Tone.UserMedia();   //  get microphone input
   recorder = new Tone.Recorder();   //  set up Tone recorder object
 
@@ -68,77 +250,13 @@ function createButtons() {
     player.start();
   }) 
 }
-
-
-
-
-
-
-
-
-
-/*
-let testButton, testButtonX=0.5 * w, testButtonY=0.5 * h, testButtonWidth=0.1 * w, testButtonHeight=0.1 * h;
-let osc;
-let testToneActive = false;
-let lfo;
-let wave;
 */
 
 
-/*  //  THIS WAS TEST CODE TO GET AMP MOD WORKING ---- IN SETUP()
-
-  testButton = createButton('TEST TONE');
-  testButton.position(testButtonX, testButtonY);
-  testButton.size(testButtonWidth, testButtonHeight);
-  testButton.mousePressed(testTone);
 
 
-  osc = new Tone.Oscillator().toDestination();  //  test oscillator
-  wave = new Tone.Waveform();
-  wave.size = 2048;
-  osc.connect(wave);
-  lfo = new Tone.LFO(1, -60, 0);
-  lfo.connect(osc.volume);
 
 
-*/
 
 
-/*    //  THIS WAS TEST CODE TO GET AMP MOD WORKING ---- IN DRAW()
-    osc.frequency.value = map(mouseX, 0, w, 30, 500);
 
-    stroke(255);
-    strokeWeight(2);
-    noFill();
-    let buffer = wave.getValue(0);
-    beginShape()
-    for (let i = 0; i < buffer.length; i++) {
-      let x = map(i, 0, buffer.length, 0, w);
-      let y = map(buffer[i], -1, 1, h, 0);
-
-      //point(x, y);
-      vertex(x,y);
-    }
-    endShape();
-*/
-
-/*
-function testTone() {
-  if (!testToneActive) {
-    console.log('BUTTON PRESSED');
-    osc.start("+0.01");
-    osc.volume.value = -70;
-
-    lfo.start();
-
-    testToneActive = true;
-  }
-  else {
-    console.log('BUTTON PRESSED AGAIN');
-    osc.stop("+0.05");
-
-    testToneActive = false;
-  }
-}
-*/
