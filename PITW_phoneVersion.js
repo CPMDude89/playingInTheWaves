b4e7101646e2565f98d51566311ee581fc5a4df4
CPMDude89 <cdbasses@gmail.com>
2021-08-16 15:40:01 +0100

    Added more buttons

--- PITW_phoneVersion.js
+++ PITW_phoneVersion.js
@@ -50,13 +50,12 @@ function setup() {
     controls2.delayTimeLFO.frequency.value = 0.08;
     controls2.delayTimeLFO.min = 0.03;
     controls2.delayTimeLFO.max = 1;
-    controls2.playbackRateIncrement = 0.02;
+    controls2.playbackRateIncrement = 0.005;
 
     controls1.connectToBus(effectBus);
     controls2.connectToBus(effectBus);
 
-    stockSample1Active = false;
-    sampler1.sampleActiveArray.push(stockSample1Active);
+    sampler1.sampleActiveArray.push(false);
     stockSample1Button = createButton('SAMPLE 1');
     stockSample1Button.position(recButX + (recButWd * 0.55), recButY + (1.05 * recButHt));
     stockSample1Button.size(recButWd * 0.2, recButHt * 0.45);
@@ -70,8 +69,7 @@ function setup() {
     })
     sample1Signal = new SignalCircle(recButX + (recButWd * 0.52), (recButHt * 0.225) + recButY + (1.05 * recButHt), 0.3 * recButHt);
 
-    stockSample2Active = false;
-    sampler1.sampleActiveArray.push(stockSample2Active);
+    sampler1.sampleActiveArray.push(false);
     stockSample2Button = createButton('SAMPLE 2');
     stockSample2Button.position(recButX + (recButWd * 0.55), recButY + (1.52 * recButHt));
     stockSample2Button.size(recButWd * 0.2, recButHt * 0.45);
@@ -85,8 +83,7 @@ function setup() {
     })
     sample2Signal = new SignalCircle(recButX + (recButWd * 0.52), (recButHt * 0.225) + recButY + (1.52 * recButHt), 0.3 * recButHt);
 
-    stockSample3Active = false;
-    sampler1.sampleActiveArray.push(stockSample3Active);
+    sampler1.sampleActiveArray.push(false);
     stockSample3Button = createButton('SAMPLE 3');
     stockSample3Button.position(recButX + (recButWd * 0.79), recButY + (1.05 * recButHt));
     stockSample3Button.size(recButWd * 0.2, recButHt * 0.45);
@@ -100,8 +97,7 @@ function setup() {
     })
     sample3Signal = new SignalCircle(recButX + (recButWd * 1.02), (recButHt * 0.225) + recButY + (1.05 * recButHt), 0.3 * recButHt);
 
-    stockSample4Active = false;
-    sampler1.sampleActiveArray.push(stockSample4Active);
+    sampler1.sampleActiveArray.push(false);
     stockSample4Button = createButton('SAMPLE 4');
     stockSample4Button.position(recButX + (recButWd * 0.79), recButY + (1.52 * recButHt));
     stockSample4Button.size(recButWd * 0.2, recButHt * 0.45);
@@ -115,6 +111,62 @@ function setup() {
     })
     sample4Signal = new SignalCircle(recButX + (recButWd * 1.02), (recButHt * 0.225) + recButY + (1.52 * recButHt), 0.3 * recButHt);
 
+    sampler2.sampleActiveArray.push(false);
+    stockSample5Button = createButton('SAMPLE 1');
+    stockSample5Button.position(recButX + (recButWd * 0.55), (recButY + (recButHt * 2.1)) + (1.05 * recButHt));
+    stockSample5Button.size(recButWd * 0.2, recButHt * 0.45);
+    stockSample5Button.mousePressed(() => {
+        sampler2.clearSampleActivity(0);
+        sampler2.player.load("./sounds/groove_clip.wav");
+        sampler2.button.html('PLAY SAMPLE 1');
+        sampler2.sampleLoaded = true;
+        sampler2.showControls();
+        sampler2.state = 'play';
+    })
+    sample5Signal = new SignalCircle(recButX + (recButWd * 0.52), (recButHt * 0.225) + (recButY + (recButHt * 2.1)) + (1.05 * recButHt), 0.3 * recButHt);
+
+    sampler2.sampleActiveArray.push(false);
+    stockSample6Button = createButton('SAMPLE 2');
+    stockSample6Button.position(recButX + (recButWd * 0.55), (recButY + (recButHt * 2.1)) + (1.52 * recButHt));
+    stockSample6Button.size(recButWd * 0.2, recButHt * 0.45);
+    stockSample6Button.mousePressed(() => {
+        sampler2.clearSampleActivity(1);
+        sampler2.player.load("./sounds/martina_3.wav");
+        sampler2.button.html('PLAY SAMPLE 2');
+        sampler2.sampleLoaded = true;
+        sampler2.showControls();
+        sampler2.state = 'play';
+    })
+    sample6Signal = new SignalCircle(recButX + (recButWd * 0.52), (recButHt * 0.225) + (recButY + (recButHt * 2.1)) + (1.52 * recButHt), 0.3 * recButHt);
+
+    sampler2.sampleActiveArray.push(false);
+    stockSample7Button = createButton('SAMPLE 3');
+    stockSample7Button.position(recButX + (recButWd * 0.79), (recButY + (recButHt * 2.1)) + (1.05 * recButHt));
+    stockSample7Button.size(recButWd * 0.2, recButHt * 0.45);
+    stockSample7Button.mousePressed(() => {
+        sampler2.clearSampleActivity(2);
+        sampler2.player.load("./sounds/Mike_Hayes_beat.wav");
+        sampler2.button.html('PLAY SAMPLE 3');
+        sampler2.sampleLoaded = true;
+        sampler2.showControls();
+        sampler2.state = 'play';
+    })
+    sample7Signal = new SignalCircle(recButX + (recButWd * 1.02), (recButHt * 0.225) + (recButY + (recButHt * 2.1)) + (1.05 * recButHt), 0.3 * recButHt);
+
+    sampler2.sampleActiveArray.push(false);
+    stockSample8Button = createButton('SAMPLE 4');
+    stockSample8Button.position(recButX + (recButWd * 0.79), (recButY + (recButHt * 2.1)) + (1.52 * recButHt));
+    stockSample8Button.size(recButWd * 0.2, recButHt * 0.45);
+    stockSample8Button.mousePressed(() => {
+        sampler2.clearSampleActivity(3);
+        sampler2.player.load("./sounds/birds_acaracle.wav");
+        sampler2.button.html('PLAY SAMPLE 4');
+        sampler2.sampleLoaded = true;
+        sampler2.showControls();
+        sampler2.state = 'play';
+    })
+    sample8Signal = new SignalCircle(recButX + (recButWd * 1.02), (recButHt * 0.225) + (recButY + (recButHt * 2.1)) + (1.52 * recButHt), 0.3 * recButHt);
+    
     Tone.Transport.start();
 }
 
@@ -158,7 +210,22 @@ function draw() {
     if (sampler1.sampleActiveArray[3] == true) {
         sample4Signal.drawActiveCircle();
     }
-    
+
+    if (sampler2.sampleActiveArray[0] == true) {
+        sample5Signal.drawActiveCircle();
+    }
+
+    if (sampler2.sampleActiveArray[1] == true) {
+        sample6Signal.drawActiveCircle();
+    }
+
+    if (sampler2.sampleActiveArray[2] == true) {
+        sample7Signal.drawActiveCircle();
+    }
+
+    if (sampler2.sampleActiveArray[3] == true) {
+        sample8Signal.drawActiveCircle();
+    }
 
     controls1.checkForActivity();
     controls2.checkForActivity();
