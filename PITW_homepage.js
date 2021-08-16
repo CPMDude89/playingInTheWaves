fb29749154766f34edbc98a2e2d0fb65cabf92e9
CPMDude89 <cdbasses@gmail.com>
2021-08-16 22:02:41 +0100

    Update PITW_homepage.js

--- PITW_homepage.js
+++ PITW_homepage.js
@@ -5,15 +5,22 @@
  */
 
 let w=window.innerWidth, h=window.innerHeight;
-let tour;
+let tour, playground, phone;
 
 function setup() {
     createCanvas(w, h);
 
     tour = createA("https://cpmdude89.github.io/playingInTheWaves/TourPlayRate.html", 'Take the TOUR!');
-    tour.position(0.5 * w, 0.5 * h);
-    tour.style('font-size', '50px');
-    tour.style('text-align', 'center');
+    tour.position(0.15 * w, 0.34 * h);
+    tour.style('font-size', '3vw');
+
+    playground = createA('https://cpmdude89.github.io/playingInTheWaves/PITW_Playground.html', 'Go to the Playground!');
+    playground.position(w * 0.62, h * 0.34);
+    playground.style('font-size', '3vw');
+
+    phone = createA("https://cpmdude89.github.io/playingInTheWaves/PITW_phoneVersion.html", 'Phone Version!');
+    phone.position(w * 0.38, h * 0.65);
+    phone.style('font-size', '4vw');
 }
 
 function draw() {
@@ -21,15 +28,12 @@ function draw() {
     
     noStroke();
     fill(112, 219, 112);
-    //rect(0, 0, w * 0.33, h * 0.8);
     rect(0, 0, w * 0.5, h * 0.6);
 
     fill(71, 209, 71);
-    //rect(w * 0.33, 0, w * 0.34, h * 0.8);
     rect(w * 0.5, 0, w * 0.5, h * 0.6);
 
-    fill(46, 184, 46);
-    //rect(w * 0.67, 0, w * 0.33, h * 0.8);
+    fill(0, 150, 80);
     rect(0, h * 0.6, w, h * 0.2);
 
     fill(0);
@@ -39,9 +43,11 @@ function draw() {
     textAlign(CENTER);
     fill(0);
     textSize(w * 0.03);
-    text('Playing In The Waves: A Play-Based Approach to Web Audio', 0.5 * w, 0.1 * h);    
+    text('Playing In The Waves:\nA Play-Based Approach to Web Audio', 0.5 * w, 0.08 * h);    
 
-    textSize(20)
+    textSize(w * 0.02)
+    text('New to Sound Design?', w * 0.25, h * 0.3);
 
+    text('Ready to Jump Right In?', w * 0.75, h * 0.3);
 
 }
