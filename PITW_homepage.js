2759c19e65dfd0d375aa9f755e3cf3049ed082dd
CPMDude89 <cdbasses@gmail.com>
2021-08-16 22:37:01 +0100

    Updated homepage

--- PITW_homepage.js
+++ PITW_homepage.js
@@ -5,7 +5,7 @@
  */
 
 let w=window.innerWidth, h=window.innerHeight;
-let tour, playground, phone;
+let tour, playground, phone, webEditor, github, blog;
 
 function setup() {
     createCanvas(w, h);
@@ -21,22 +21,34 @@ function setup() {
     phone = createA("https://cpmdude89.github.io/playingInTheWaves/PITW_phoneVersion.html", 'Phone Version!');
     phone.position(w * 0.38, h * 0.65);
     phone.style('font-size', '4vw');
+
+    webEditor = createA("https://editor.p5js.org/CPMDude89/sketches/mlk0pa0YD", 'p5 Web Editor');
+    webEditor.position(w * 0.1, h * 0.85);
+    webEditor.style('font-size', '3vw');
+
+    github = createA("https://github.com/CPMDude89/playingInTheWaves", 'GitHub');
+    github.position(w * 0.45, h * 0.85);
+    github.style('font-size', '3vw');
+
+    blog = createA("https://chrisduvallmusic.com/2021/05/20/beginning/", 'Blog');
+    blog.position(w * 0.8, h * 0.85);
+    blog.style('font-size', '3vw');
 }
 
 function draw() {
     background(255);
     
     noStroke();
-    fill(112, 219, 112);
+    fill(112, 219, 120);
     rect(0, 0, w * 0.5, h * 0.6);
 
-    fill(71, 209, 71);
+    fill(71, 209, 80);
     rect(w * 0.5, 0, w * 0.5, h * 0.6);
 
     fill(0, 150, 80);
     rect(0, h * 0.6, w, h * 0.2);
 
-    fill(0);
+    fill(153, 255, 255);
     rect(0, h * 0.8, w, h * 0.2);
 
     noStroke();
@@ -49,5 +61,6 @@ function draw() {
     text('New to Sound Design?', w * 0.25, h * 0.3);
 
     text('Ready to Jump Right In?', w * 0.75, h * 0.3);
+
 
 }
