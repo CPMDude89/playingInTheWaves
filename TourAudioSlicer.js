2e7f60da9968a9153ae3d887ece2c1eb0186461e
CPMDude89 <cdbasses@gmail.com>
2021-08-19 18:26:43 +0100

    Finishing touches on Audio Slicer!

--- TourAudioSlicer.js
+++ TourAudioSlicer.js
@@ -9,7 +9,8 @@
 let w=window.innerWidth, h=window.innerHeight;
 let mic, recorder, recordButton, data, blob, player, player2;
 let recButX=(0.08 * w), recButY=(0.14 * h), recButWd=(0.1 * w), recButHt=(0.1 * h);
-let sampButX=(0.9 * w), sampButY=(0.1 * h), sampButWd=(0.05 * w), sampButHt=(0.08 * h);
+let samp1ButX=(0.9 * w), sampButWd=(0.07 * w), sampButHt=(0.08 * h);
+let samp1ButY=(0.15 * h), samp2ButY=(0.3 * h), samp3ButY=(0.45 * h), samp4ButY=(0.6 * h);
 let clearBut;
 let state = 0;
 let bufferArray;
@@ -26,14 +27,19 @@ let linkForward, linkBackward;
 let loopStartPoint, loopLength, clip;
 let limiter;
 let newBuffer, newFileButton;
-let sample1Active=false, sample2Active=false;
+let sample1Active=false, sample2Active=false, sample3Active=false, sample4Active=false;
 let pageRecorder, pageRecButX = w * 0.9, pageRecButY = 0.85 * h, pageRecButWd=0.65 * recButWd;
 
 function preload() {
+    //  volume nodes
     volNode = new Tone.Volume().toDestination();
     limiter = new Tone.Limiter(-2).connect(volNode);
-    sample1 = new Tone.ToneAudioBuffer("./sounds/groove_clip.wav");
-    sample2 = new Tone.ToneAudioBuffer("./sounds/metal_water_bottle.wav");
+
+    //  stock samples
+    sample1 = new Tone.ToneAudioBuffer("./sounds/metal_water_bottle.wav");
+    sample2 = new Tone.ToneAudioBuffer("./sounds/CD_jump.wav");
+    sample3 = new Tone.ToneAudioBuffer("./sounds/slap2.wav");
+    sample4 = new Tone.ToneAudioBuffer("./sounds/bassOrbit2.wav");
 }
 
 function setup() {
@@ -50,16 +56,28 @@ function setup() {
     recordButton.size(recButWd, recButHt);
     recordButton.mousePressed(recordIn);    
 
+    //  stock sample buttons
     sample1Button = createButton('SAMPLE 1');
-    sample1Button.position(sampButX, sampButY);
+    sample1Button.position(samp1ButX, samp1ButY);
     sample1Button.size(sampButWd, sampButHt);
     sample1Button.mousePressed(triggerSample1);
 
     sample2Button = createButton('SAMPLE 2');
-    sample2Button.position(sampButX, 2.5 * sampButY);
+    sample2Button.position(samp1ButX, samp2ButY);
     sample2Button.size(sampButWd, sampButHt);
     sample2Button.mousePressed(triggerSample2);
 
+    sample3Button = createButton('SAMPLE 3');
+    sample3Button.position(samp1ButX, samp3ButY);
+    sample3Button.size(sampButWd, sampButHt);
+    sample3Button.mousePressed(triggerSample3);
+
+    sample4Button = createButton('SAMPLE 4');
+    sample4Button.position(samp1ButX, samp4ButY);
+    sample4Button.size(sampButWd, sampButHt);
+    sample4Button.mousePressed(triggerSample4);
+
+    //  navigational links
     linkForward = createA('https://cpmdude89.github.io/playingInTheWaves/PITW_Playground.html', 'ON TO THE PLAYGROUND!');
     linkForward.position(0.75 * w, 0.04 * h);
     linkForward.style('font-size', '1.5vw');
@@ -68,8 +86,10 @@ function setup() {
     linkBackward.position(0.05 * w, 0.04 * h);
     linkBackward.style('font-size', '1.5vw');
 
-    clip = new Tone.Loop(playclip, 0.3);    //  this is the loop to time granulation
+    //  this is the loop to time granulation
+    clip = new Tone.Loop(playclip, 0.3);    
 
+    //  page recorder to grab page's audio out
     pageRecorder = new PageRecorder(pageRecButX, pageRecButY, pageRecButWd, recButHt);
     volNode.connect(pageRecorder.recorder);
     pageRecorderSignal = new SignalCircle(pageRecButX + (0.5 * pageRecButWd), pageRecButY - (0.5 * recButHt), 0.4 * recButHt);
@@ -87,19 +107,10 @@ function draw() {
     fill(0);       
     text('Playing In The Waves:\nAudio Slicing', 0.5 * w, 0.05 * h); //  page title
 
-    if (state == 1) {     //  if button is recording
-        fill(255, 0, 0);    //  red for record light
+    //  signal circles
+    if (state == 1) {     //    signal circle for main 'samplerButton'
+        fill(255, 0, 0);    
         circle((recButX + (1.25 * recButWd)), (recButY + (0.5 * recButHt)), 0.4 * recButHt);
-    }
-
-    if (sample1Active) {
-        fill(0, 0, 255);
-        circle(sampButX + (0.5 * sampButWd), sampButY - (0.4 * sampButHt), 0.4 * recButHt);
-    }
-
-    if (sample2Active) {
-        fill(0, 0, 255);
-        circle(sampButX + (0.5 * sampButWd), 2.5 * sampButY - (0.4 * sampButHt), 0.4 * recButHt);
     }
 
     if (pageRecorder.state == 'recording') {
@@ -115,19 +126,21 @@ function draw() {
         stroke(255);
         strokeWeight(1);
 
+        //  draw waveform
         beginShape();
         for (let i = 0; i < player.buffer.toArray(0).length; i += 300) {
 
-            let x = map(i, 0, player.buffer.toArray(0).length, leftSide, rightSide);
-            let y = map(player.buffer.toArray(0)[i], -1, 1, bottomSide, topSide);
+            let x = map(i, 0, player.buffer.toArray(0).length, leftSide, rightSide);    //  get x coordinate
+            let y = map(player.buffer.toArray(0)[i], -1, 1, bottomSide, topSide);       //  get y coordinate
 
-            vertex(x, y);
+            vertex(x, y);   //  draw point at coordinate
         }
-        endShape();
+        endShape(); //  end drawing waveform
 
         textSize(30);
         noStroke();
-        if (playActive) {
+        //  trigger textual guides when audio is stored in buffer
+        if (playActive || sample1Active || sample2Active || sample3Active || sample4Active) {
             text('Click anywhere on black box to STOP playback.', 0.5 * w, 0.85 * topSide);
             strokeWeight(5);
             stroke(0);
@@ -182,14 +195,15 @@ function draw() {
         }   
     }
 
+    //  draw control lines
     stroke(255, 0, 0);
     strokeWeight(1.5);
         line(startLine, topSide, startLine, bottomSide); //  start line
         line(endLine, topSide, endLine, bottomSide);   //  end line
-
-    
 }
 
+
+//  basically a SamplerButton, but with some modifications
 async function recordIn() {
     if (state == 0 || state == 3) {       //  begin recording
         if (clip.state == 'started') {
@@ -207,19 +221,23 @@ async function recordIn() {
         data = await recorder.stop();   //  end recording and return a javascript promise with the result in it
         blob = URL.createObjectURL(data);   //  save the result of the recoder object into a blob and assign it a url object
         player = new Tone.Player(blob).connect(limiter);  //  connect recording to Tone player and route player to master output
-        player.fadeIn = 0.02;
+        player.fadeIn = 0.02;   //  actually using player fades here
         player.fadeOut = 0.02;
         
-        showControls();
+        showControls();     //  create start over button
 
         recordButton.html('PLAY RECORDING');    //  change button text
         state = 2;
 
+        //  if a stock sample is active, turn them all off just in case
         sample1Active = false;
         sample2Active = false;
+        sample3Active = false;
+        sample4Active = false;
     }
 }
 
+//  make start over button and hide record button
 function showControls() {
     recordButton.hide();
 
@@ -237,6 +255,7 @@ function showControls() {
     });
 }
 
+//  trigger audio process with mouse click
 function mouseClicked() {
     if (mouseX >= leftSide && mouseX <= rightSide && mouseY >= topSide && mouseY <= bottomSide && state > 1) {
         if (!playActive) {
@@ -261,20 +280,48 @@ function playclip(time) {
         clip.interval = loopLength;
 }
 
+
+//  stock sample functions
 function triggerSample1() {
     state = 3;
-    player = new Tone.Player(sample1).connect(limiter);  //  connect recording to Tone player and route player to master output
+    player = new Tone.Player(sample1).connect(limiter);  
     player.fadeIn = 0.02;
     player.fadeOut = 0.02;
     sample1Active = true;
     sample2Active = false;
+    sample3Active = false;
+    sample4Active = false;
 }
 
 function triggerSample2() {
     state = 3;
-    player = new Tone.Player(sample2).connect(limiter);  //  connect recording to Tone player and route player to master output
+    player = new Tone.Player(sample2).connect(limiter);  
     player.fadeIn = 0.02;
     player.fadeOut = 0.02;
     sample1Active = false;
     sample2Active = true;
+    sample3Active = false;
+    sample4Active = false;
+}
+
+function triggerSample3() {
+    state = 3;
+    player = new Tone.Player(sample3).connect(limiter);  
+    player.fadeIn = 0.02;
+    player.fadeOut = 0.02;
+    sample1Active = false;
+    sample2Active = false;
+    sample3Active = true;
+    sample4Active = false;
+}
+
+function triggerSample4() {
+    state = 3;
+    player = new Tone.Player(sample4).connect(limiter);  
+    player.fadeIn = 0.02;
+    player.fadeOut = 0.02;
+    sample1Active = false;
+    sample2Active = false;
+    sample3Active = false;
+    sample4Active = true;
 }
